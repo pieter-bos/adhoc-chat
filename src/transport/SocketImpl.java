c6c3c0b9ff7c143188ab82d5d4792813cd1691dd
Magnificus36 <magnificus@outlook.com>
2014-04-07 12:28:18 +0200

    Implemented SocketImpl constructor

--- src/transport/SocketImpl.java
+++ src/transport/SocketImpl.java
@@ -1,11 +1,21 @@
 package transport;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 public class SocketImpl implements Socket {
+    private static final String GROUP = "224.224.224.224";
+    private MulticastSocket mulSock;
+
+    public SocketImpl(int port) throws IOException {
+        mulSock = new MulticastSocket(port);
+        mulSock.joinGroup(InetAddress.getByName(GROUP));
+    }
+
     @Override
     public void connect() {
-        
+
     }
 
     @Override
