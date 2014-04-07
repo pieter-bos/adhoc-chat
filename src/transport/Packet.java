00520743224d1f9f6be1d7d24b8ee6204a199ced
Magnificus36 <magnificus@outlook.com>
2014-04-07 11:30:05 +0200

    Added getSource- & getDestinationAddress() and length() to packet interface

--- src/transport/Packet.java
+++ src/transport/Packet.java
@@ -1,7 +1,15 @@
 package transport;
 
+import java.net.InetAddress;
+
 public interface Packet {
     // public Packet(byte[] data);
 
+    public InetAddress getSourceAddress();
+
+    public InetAddress getDestinationAddress();
+
     public byte[] getData();
+
+    public int length();
 }
