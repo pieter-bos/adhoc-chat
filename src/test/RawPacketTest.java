d218ae270bb3356a33eb109971c31b5e7b1923ff
Magnificus36 <magnificus@outlook.com>
2014-04-07 23:48:03 +0200

    Added assertEquals empty array in RawPacketTest

--- src/test/RawPacketTest.java
+++ src/test/RawPacketTest.java
@@ -8,6 +8,7 @@ public class RawPacketTest {
     public void testGetData() throws Exception {
         RawPacket rp = new RawPacket((byte) 0, 0, 0, new byte[] {1, 2, 3, 4}, new byte[] {1, 2, 3, 4}, null);
         org.junit.Assert.assertNotNull(rp.getData());
+        org.junit.Assert.assertEquals(new byte[] {}, rp.getData());
 
         byte[] test = new byte[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         rp = new RawPacket((byte) 0, 0, 0, new byte[] {1, 2, 3, 4}, new byte[] {1, 2, 3, 4}, test);
