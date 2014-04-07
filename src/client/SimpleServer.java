59ccc26e907e1228689250219e23668d1b3b551a
Laurens Rouw <jlk.rouw@gmail.com>
2014-04-07 20:39:09 +0200

    Added Simple server library and very basic implementation

--- src/client/SimpleServer.java
+++ src/client/SimpleServer.java
@@ -1,13 +1,48 @@
 package client;
 
 import com.sun.net.httpserver.HttpServer;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
+import org.simpleframework.http.core.ContainerServer;
+import org.simpleframework.transport.Server;
+import org.simpleframework.transport.connect.SocketConnection;
 import sun.net.httpserver.HttpServerImpl;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+
 /**
- *
+ * Serves static files for the user interface
  */
 public class SimpleServer {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = new HttpServerImpl();
+    /**
+     * Creates a new http server that serves files on port
+     * @param port
+     */
+    public SimpleServer(int port) {
+        Container container = new Container() {
+            @Override
+            public void handle(Request request, Response response) {
+                try {
+                    PrintStream body = response.getPrintStream();
+                    long time = System.currentTimeMillis();
+
+                    response.setValue("Content-Type", "text/plain");
+                    response.setValue("Server", "HelloWorld/1.0 (Simple 4.0)");
+                    response.setDate("Date", time);
+                    response.setDate("Last-Modified", time);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        try {
+            new SocketConnection(new ContainerServer(container)).connect(new InetSocketAddress(port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
