184d8901d9efbd7e338bb727e6a294638454e72e
Magnificus36 <magnificus@outlook.com>
2014-04-07 15:16:43 +0200

    Added ReceiverThread

--- src/transport/ReceiverThread.java
+++ src/transport/ReceiverThread.java
@@ -1,7 +1,50 @@
 package transport;
 
-/**
- * Created by Student on 7-4-2014.
- */
-public class ReceiverThread {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReceiverThread extends Thread {
+    private MulticastSocket mulSocket;
+    private Set<PacketListener> packetListeners = new HashSet<PacketListener>();
+
+    public ReceiverThread(MulticastSocket socket) {
+        mulSocket = socket;
+    }
+
+    public void addPacketListener(PacketListener listener) {
+        synchronized (packetListeners) {
+            packetListeners.add(listener);
+        }
+    }
+
+    public void removePacketListener(PacketListener listener) {
+        synchronized (packetListeners) {
+            packetListeners.remove(listener);
+        }
+    }
+
+    @Override
+    public void run() {
+        byte[] buf = new byte[1<<16];
+        try {
+            while (true) {
+                DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length);
+                mulSocket.receive(datagramPacket);
+
+                // TODO Parse datagramPacket
+
+
+                synchronized (packetListeners) {
+                    for (PacketListener listener : packetListeners) {
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // STOP
+        }
+    }
 }
