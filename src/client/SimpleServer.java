6510b36d8b0415f0c393fea6847c339404cf8c82
Laurens Rouw <jlk.rouw@gmail.com>
2014-04-08 10:59:02 +0200

    Moved web resources and added non working http server implementation

--- src/client/SimpleServer.java
+++ src/client/SimpleServer.java
@@ -1,14 +1,21 @@
 package client;
 
+import org.simpleframework.http.Path;
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
 import org.simpleframework.http.core.Container;
 import org.simpleframework.http.core.ContainerServer;
 import org.simpleframework.transport.connect.SocketConnection;
+import sun.misc.IOUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 /**
  * Serves static files for the user interface
@@ -26,13 +33,35 @@ public class SimpleServer {
                     PrintStream body = response.getPrintStream();
                     long time = System.currentTimeMillis();
 
-                    response.setValue("Content-Type", "text/plain");
-                    response.setValue("Server", "HelloWorld/1.0 (Simple 4.0)");
+                    String resource = !request.getPath().getPath().equals("/")
+                            ? "/web" + request.getPath().getPath()
+                            : "/web/index.html";
+
+                    if (resource.endsWith("html")) {
+                        response.setValue("Content-Type", "text/html");
+                    } else if (resource.endsWith("css")) {
+                        response.setValue("Content-Type", "text/css");
+                    }
+
                     response.setDate("Date", time);
                     response.setDate("Last-Modified", time);
+
+                    body.append(getFileContents(resource));
+                    body.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+
+            private String getFileContents(String resource) {
+                Scanner sc = new Scanner(getClass().getResourceAsStream(resource));
+                StringBuilder out = new StringBuilder();
+
+                while (sc.hasNextLine()) {
+                    out.append(sc.nextLine());
+                }
+
+                return out.toString();
             }
         };
 
