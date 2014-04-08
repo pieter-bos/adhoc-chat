85c42e3d4882120fe56c5061b2a2274052dd3ac8
Sophie Lathouwers <s.a.m.lathouwers@student.utwente.nl>
2014-04-08 14:04:12 +0200

    Added websocket initialization to Main

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,10 +1,14 @@
 package client;
 
+import java.net.InetSocketAddress;
+
 /**
  * Entry point for the chat application
  */
 public class Main {
     public static void main(String[] args) {
         SimpleServer server = new SimpleServer(8080);
+        ClientHandler clientHandler = new ClientHandler(new InetSocketAddress(8081),null);
+        clientHandler.start();
     }
 }
