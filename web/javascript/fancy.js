3b810f4452a42367bf79ad09f6095ad4690d71c0
Sophie Lathouwers <s.a.m.lathouwers@student.utwente.nl>
2014-04-08 11:00:22 +0200

    Added javascript for tab switching

--- web/javascript/fancy.js
+++ web/javascript/fancy.js
@@ -1,4 +1,12 @@
-$('#chat-tabs a').click(function (e) {
+$('#chat-tabs a').click( function (e) {
   e.preventDefault()
   $(this).tab('show')
+})
+
+$('#user-list a').click( function(e) {
+
+})
+
+$('#comment-section').keydown(function(event){
+
 })
