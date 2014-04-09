e58d654a334723416095472f3acc1bd8eabaad52
Magnificus36 <magnificus@outlook.com>
2014-04-09 12:13:09 +0200

    Just keep hashes of forwarded packets to save memory

--- src/transport/BroadcastHandler.java
+++ src/transport/BroadcastHandler.java
@@ -8,7 +8,10 @@ import java.util.HashSet;
 public class BroadcastHandler implements PacketListener {
     private SocketImpl socket;
 
-    private HashSet<RawPacket> packets = new HashSet<>();
+    /**
+     * Hashes of already forwarded packets.
+     */
+    private HashSet<Integer> packets = new HashSet<>();
 
     public BroadcastHandler(SocketImpl socket) {
         this.socket = socket;
@@ -17,8 +20,8 @@ public class BroadcastHandler implements PacketListener {
     @Override
     public void onPacketReceived(RawPacket packet) {
         if(!Arrays.equals(packet.getDestinationAddress(), socket.getAddress().getAddress())) {
-            if(!packets.contains(packet)) {
-                packets.add(packet);
+            if(!packets.contains(packet.hashCode())) {
+                packets.add(packet.hashCode());
                 socket.send(packet);
             }
         }
