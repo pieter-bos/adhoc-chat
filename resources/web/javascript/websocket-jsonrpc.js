1f6ab45ef7ed8f763dd2672e871c89f1feacba04
Pieter Bos <info@pieterbos.me>
2014-04-09 23:54:46 +0200

    Error/Open/Close handling and event model for WebSocketJSONRPC

--- resources/web/javascript/websocket-jsonrpc.js
+++ resources/web/javascript/websocket-jsonrpc.js
@@ -1,14 +1,54 @@
 function WebSocketJSONRPC(url) {
+    this.open = false;
     this.socket = new WebSocket(url);
+
     this.callbacks = {};
+    this.events = { error: [], close: [], open: [] };
 
     this._id = 0;
+
+    this.socket.onmessage = function(e) {
+        var data = JSON.parse(e.data);
+    }
+
+    this.socket.onerror = function(e) {
+        this.emit('error', e.message);
+    }
+
+    this.socket.onopen = function(e) {
+        this.open = true;
+        this.emit('open');
+    }
+
+    this.socket.onclose = function(e) {
+        this.open = false;
+        this.emit('close');
+    }
+
+    this.on = function(event, callback) {
+        if(event in this.events) {
+            this.events[event].push(callback);
+        }
+    }
+
+    this.emit = function(event) {
+        var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+
+        for(var callback in this.events[event]) {
+            this.events[event][callback].apply(null, args);
+        }
+    }
 
     this.newId = function() {
         return this._id++;
     }
 
     this.call = function(func) {
+        if(!this.open) {
+            this.emit('error', 'Cannot call a function when the connection is not yet open.');
+            return false;
+        }
+
         var args = Array.prototype.slice.call(arguments, 1, arguments.length - 1);
         var callback = arguments[arguments.length - 1];
         var id = this.newId();
@@ -21,5 +61,7 @@ function WebSocketJSONRPC(url) {
             params: args,
             id: id
         }));
+
+        return true;
     }
 }
