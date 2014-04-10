22af0931e82733da4ccdb8f386c40e9ee2ac68db
Laurens Rouw <jlk.rouw@gmail.com>
2014-04-10 13:58:28 +0200

    Renamed HeartBeat to NckChange

--- src/client/protocol/NickChangeMessange.java
+++ src/client/protocol/NickChangeMessange.java
@@ -3,14 +3,14 @@ package client.protocol;
 /**
  * Broadcasted by client to notify others of it's presence on the network
  */
-public class HeartbeatMessage extends Message {
+public class NickChangeMessange extends Message {
     private String nick;
 
     /**
      * Constructor
      * @param nick Nickname of this client
      */
-    public HeartbeatMessage(String nick) {
+    public NickChangeMessange(String nick) {
         this.nick = nick;
     }
 }
