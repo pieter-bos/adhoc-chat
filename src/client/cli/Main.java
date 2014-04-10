2840b3695b53d2c75440a1d9066e7d451a3d1a9f
Magnificus36 <magnificus@outlook.com>
2014-04-10 15:14:23 +0200

    cli main stub

---
+++ src/client/cli/Main.java
@@ -0,0 +1,16 @@
+package client.cli;
+
+import transport.Socket;
+
+public class Main {
+
+    private Socket socket;
+
+    public Main(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
