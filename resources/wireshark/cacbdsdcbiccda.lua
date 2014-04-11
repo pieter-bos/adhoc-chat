ae5107fa832c2486d527aeb8cc6200c5fbb6566a
Pieter Bos <info@pieterbos.me>
2014-04-11 15:57:20 +0200

    Minor fix

--- resources/wireshark/cacbdsdcbiccda.lua
+++ resources/wireshark/cacbdsdcbiccda.lua
@@ -9,8 +9,8 @@ function caa.dissector(buffer, pinfo, tree)
     subtree:add(buffer(1, 4), "Sequence Number: " .. buffer(1, 4):uint())
     subtree:add(buffer(5, 4), "Acknowledgement Number: " .. buffer(5, 4):uint())
     subtree:add(buffer(9, 4), "Retransmission Number: " .. buffer(9, 4):uint())
-    subtree:add(buffer(13, 4), "Source address: " .. buffer(13, 4):ipv4())
-    subtree:add(buffer(17, 4), "Destination address: " .. buffer(17, 4):ipv4())
+    subtree:add(buffer(13, 4), "Source address: " .. buffer(13, 1):uint() .. "." .. buffer(14, 1):uint() .. "." .. buffer(15, 1):uint() .. "." .. buffer(16, 1):uint())
+    subtree:add(buffer(17, 4), "Destination address: " .. buffer(17, 1):uint() .. "." .. buffer(18, 1):uint() .. "." .. buffer(19, 1):uint() .. "." .. buffer(20, 1):uint())
     subtree:add(buffer(21, buffer:len() - 21), "Payload")
 end
 
