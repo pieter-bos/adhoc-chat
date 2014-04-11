fd0c1244389423f2226e21fc379806ee0fd6a4cc
Pieter Bos <info@pieterbos.me>
2014-04-11 15:52:52 +0200

    Enhanced dissector

--- resources/wireshark/cacbdsdcbiccda.lua
+++ resources/wireshark/cacbdsdcbiccda.lua
@@ -5,9 +5,13 @@ function caa.dissector(buffer, pinfo, tree)
 
     local subtree = tree:add(caa, buffer(), "CAA Protocol Data")
 
-    subtree:add(buffer(0, 1), "Flags: " .. buffer(0, 1):uint())
+    subtree:add(buffer(0, 1), "Flags: " .. buffer(0, 1):bitfield())
     subtree:add(buffer(1, 4), "Sequence Number: " .. buffer(1, 4):uint())
-    subtree:add(buffer(5, 4), "Acknowledgement Number" .. buffer(5, 4):uint())
+    subtree:add(buffer(5, 4), "Acknowledgement Number: " .. buffer(5, 4):uint())
+    subtree:add(buffer(9, 4), "Retransmission Number: " .. buffer(9, 4):uint())
+    subtree:add(buffer(13, 4), "Source address: " .. buffer(13, 4):ipv4())
+    subtree:add(buffer(17, 4), "Destination address: " .. buffer(17, 4):ipv4())
+    subtree:add(buffer(21, buffer:len() - 21), "Payload")
 end
 
 udp_table = DissectorTable.get("udp.port")
