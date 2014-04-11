2873dcec1df027861236b75d39ef8b2d79e10267
Pieter Bos <info@pieterbos.me>
2014-04-11 15:59:55 +0200

    Minor fix

--- resources/wireshark/cacbdsdcbiccda.lua
+++ resources/wireshark/cacbdsdcbiccda.lua
@@ -5,7 +5,7 @@ function caa.dissector(buffer, pinfo, tree)
 
     local subtree = tree:add(caa, buffer(), "CAA Protocol Data")
 
-    subtree:add(buffer(0, 1), "Flags: " .. buffer(0, 1):bitfield())
+    subtree:add(buffer(0, 1), "Flags: " .. buffer(0, 1):bitfield(0, 8))
     subtree:add(buffer(1, 4), "Sequence Number: " .. buffer(1, 4):uint())
     subtree:add(buffer(5, 4), "Acknowledgement Number: " .. buffer(5, 4):uint())
     subtree:add(buffer(9, 4), "Retransmission Number: " .. buffer(9, 4):uint())
