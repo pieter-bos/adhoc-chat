5c5bb624392dc8f733b0f96adfbf6b5d20843a95
Laurens Rouw <jlk.rouw@gmail.com>
2014-04-13 17:20:03 +0200

    Added logic for choosing nicknames

--- resources/web/javascript/app.js
+++ resources/web/javascript/app.js
@@ -3,6 +3,24 @@ var chat = angular.module('chat', [])
 .service('websocketService', function($rootScope) {
 
 })
+// Provides application settings
+.service('settingService', function($rootScope) {
+    this.nickname = 'choose a nickname...';
+
+    this.updateNickname = function(nickname) {
+        this.nickname = nickname;
+        $rootScope.$broadcast('settingService::nicknameChanged');
+    }
+
+    this.init = function() {
+        $('#nick-modal').modal({ keyboard: false, backdrop: 'static' });
+        $('#nick-modal form').on('submit', function(event) {
+            $('#nick-modal').modal('hide');
+        });
+    }
+
+    this.init();
+})
 // Provides functions to manipulate user model
 .service('userModel', function(websocketService) {
 
@@ -10,6 +28,21 @@ var chat = angular.module('chat', [])
 // Provides function to manipulate conversation model
 .service('conversationModel', function(websocketService) {
 
+})
+// Controller for application settings
+.controller('settingController', function($scope, settingService) {
+    $scope.nickname = settingService.nickname;
+
+    $scope.nicknameField = '';
+
+    $scope.updateNickname = function() {
+        settingService.updateNickname($scope.nicknameField);
+        $scope.nicknameField = '';
+    }
+
+    $scope.$on('settingService::nicknameChanged', function() {
+        $scope.nickname = settingService.nickname;
+    });
 })
 // Controller for user related views
 .controller('userController', function($scope, userModel) {
