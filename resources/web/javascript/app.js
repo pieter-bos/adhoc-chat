140d9edf6c647908225b96075bcb3b66fa65c9a3
Laurens Rouw <jlk.rouw@gmail.com>
2014-04-13 16:48:46 +0200

    Added angular application skeleton

--- resources/web/javascript/app.js
+++ resources/web/javascript/app.js
@@ -1,1 +1,21 @@
-var chat = angular.module('chat', []);
+var chat = angular.module('chat', [])
+// Provides functions for websocket communication
+.service('websocketService', function($rootScope) {
+
+})
+// Provides functions to manipulate user model
+.service('userModel', function(websocketService) {
+
+})
+// Provides function to manipulate conversation model
+.service('conversationModel', function(websocketService) {
+
+})
+// Controller for user related views
+.controller('userController', function($scope, userModel) {
+
+})
+// Controller for conversation related views
+.controller('conversationController', function($scope, conversationModel) {
+
+});
