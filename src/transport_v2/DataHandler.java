0b970bd979dfba9744d29643c90ee0fc59979456
Magnificus36 <magnificus@outlook.com>
2014-04-14 23:09:14 +0200

    Added check if SYN seq# is within bounds

--- src/transport_v2/DataHandler.java
+++ src/transport_v2/DataHandler.java
@@ -1,13 +1,18 @@
 package transport_v2;
 
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class DataHandler implements PacketListener {
+    private static final long MAX_DIFFERENCE = 5;
     private final SocketImpl socket;
     private final LinkedBlockingQueue<Packet> queue;
-    private final SortedSet<RawPacket> outOfOrderPackets = new TreeSet<>();
+    private final HashMap<InetAddress, Integer> lastInOrderSequenceNumber = new HashMap<>();
+    private final HashMap<InetAddress, SortedSet<RawPacket>> outOfOrderPackets = new HashMap<>();
 
     public DataHandler(SocketImpl socket, LinkedBlockingQueue<Packet> queue) {
         this.socket = socket;
@@ -21,8 +26,16 @@ public class DataHandler implements PacketListener {
         }
 
         if(packet.isSyn()) {
-            for(RawPacket toRemove : outOfOrderPackets.tailSet(packet)) {
-                outOfOrderPackets.remove(toRemove);
+            // TODO check if lastInOrderSequenceNumber and outOfOrderPackets contains key packet.getSourceIp()
+
+            // If the sequence number of the SYN is smaller than the last in order sequence number or
+            //  there is a packet received out of order with a sequence number more than MAX_DIFFERENCE larger
+            //  then the other client is suspected of having restarted.
+            if (Util.differenceWithWrapAround(packet.getSequenceNumber(), lastInOrderSequenceNumber.get(packet.getSourceIp())) < -1 ||
+                    !outOfOrderPackets.get(packet.getSourceIp()).tailSet(packet).isEmpty() &&
+                            Util.differenceWithWrapAround(packet.getSequenceNumber(), outOfOrderPackets.get(packet.getSourceIp()).last().getSequenceNumber()) > MAX_DIFFERENCE) {
+                lastInOrderSequenceNumber.put(packet.getSourceIp(), packet.getSequenceNumber());
+                outOfOrderPackets.put(packet.getSourceIp(), new TreeSet<RawPacket>());
             }
         }
     }
